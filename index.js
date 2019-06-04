8c3093cfe5e1deb598e58162cde02e3aaaeac0cf
Arjun Mehta <arjunmeht@gmail.com>
2019-06-04 08:43:22 -0700

    Set default worker settings in index

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const os = require('os');
 const yargs = require('yargs');
 const MultiWriteable = require('./lib/multi-writable');
 
@@ -5,10 +6,12 @@ const { argv } = yargs
   .option('dir', { alias: 'd' })
   .option('workers', { alias: 'w' });
 
+
 const writeStream = new MultiWriteable({
-  numWorkers: argv.workers,
+  numWorkers: argv.workers || Math.round(os.cpus().length / 2) - 1 || 1,
   workerConfig: { outputDir: argv.dir },
 });
+
 
 process
   .stdin
