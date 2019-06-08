41324a0c6cda1fa416d435cc2ae379eee092d0f1
Arjun Mehta <arjunmeht@gmail.com>
2019-06-08 12:59:07 -0700

    Match documented interface

--- index.js
+++ index.js
@@ -1,15 +1,14 @@
-const os = require('os');
 const yargs = require('yargs');
 const MultiWriteable = require('./lib/multi-writable');
 
 const { argv } = yargs
-  .option('dir', { alias: 'd' })
+  .option('dir-output', { alias: 'd' })
   .option('workers', { alias: 'w' });
 
 
 const writeStream = new MultiWriteable({
-  numWorkers: argv.workers || Math.round(os.cpus().length / 2) - 1 || 1,
-  workerConfig: { outputDir: argv.dir },
+  numWorkers: argv.workers || 1,
+  workerConfig: { outputDir: argv['dir-output'] },
 });
 
 
