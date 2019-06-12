6cf079c425486b1ec1611333cb05bdb471ffc2b7
Arjun Mehta <arjunmeht@gmail.com>
2019-06-12 15:19:47 -0700

    Add descriptions to help content

--- cli.js
+++ cli.js
@@ -3,30 +3,57 @@
 // Copyright (c) 2019 Arjun Mehta
 // MIT License
 
-
 const fs = require('fs');
 const path = require('path');
 const yargs = require('yargs');
 const SQLConverterStream = require('./lib/sql-converter-stream');
+const packageConfig = require('./package.json');
+
+
+// Read in options
 
 const {
-  'dir-output': dirOutput,
+  'dir-output': outputDir,
   input,
   workers,
   schema,
 } = yargs
-  .option('dir-output', { alias: 'd' })
-  .option('input', { alias: 'i' })
-  .option('workers', { alias: 'w' })
-  .option('schema', { alias: 's' })
+  .version(packageConfig.version)
+  .help('help')
+  .alias('help', 'h')
+  .option('dir-output', {
+    alias: 'd',
+    description: 'Disable stdout and output to a directory',
+    string: true,
+  })
+  .option('input', {
+    alias: 'i',
+    description: 'Disable stdin and read from a dumpfile instead',
+    string: true,
+  })
+  .option('workers', {
+    alias: 'w',
+    description: 'Add extra workers and split their output',
+    number: true,
+  })
+  .option('schema', {
+    alias: 's',
+    description: 'Output the detected schema as a JSON file',
+    boolean: true,
+  })
   .argv;
 
 
+// Initialize main SQL Converter writable stream
+
 const converter = new SQLConverterStream({
-  numWorkers: dirOutput ? parseInt(workers || 1, 10) : 1,
-  outputDir: dirOutput,
+  outputDir,
+  numWorkers: outputDir ? parseInt(workers || 1, 10) : 1,
   schemaFormat: schema,
 });
+
+
+// Pipe file or stdin
 
 if (input) {
   const filePath = path.resolve(__dirname, input);
