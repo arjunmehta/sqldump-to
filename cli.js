0a1ab77ee6326dbed4c29c483096ab7eadfdf1b2
Arjun Mehta <arjunmehta@users.noreply.github.com>
2019-06-12 15:22:16 -0700

    Support input file specification and enhance cli help content (#23)
    * Support input file specification
    
    * Add descriptions to help content

--- cli.js
+++ cli.js
@@ -3,26 +3,74 @@
 // Copyright (c) 2019 Arjun Mehta
 // MIT License
 
-
+const fs = require('fs');
+const path = require('path');
 const yargs = require('yargs');
 const SQLConverterStream = require('./lib/sql-converter-stream');
+const packageConfig = require('./package.json');
 
-const { argv } = yargs
-  .option('dir-output', { alias: 'd' })
-  .option('workers', { alias: 'w' })
-  .option('schema', { alias: 's' });
 
+// Read in options
+
+const {
+  'dir-output': outputDir,
+  input,
+  workers,
+  schema,
+} = yargs
+  .version(packageConfig.version)
+  .help('help')
+  .alias('help', 'h')
+  .option('dir-output', {
+    alias: 'd',
+    description: 'Disable stdout and output to a directory',
+    string: true,
+  })
+  .option('input', {
+    alias: 'i',
+    description: 'Disable stdin and read from a dumpfile instead',
+    string: true,
+  })
+  .option('workers', {
+    alias: 'w',
+    description: 'Add extra workers and split their output',
+    number: true,
+  })
+  .option('schema', {
+    alias: 's',
+    description: 'Output the detected schema as a JSON file',
+    boolean: true,
+  })
+  .argv;
+
+
+// Initialize main SQL Converter writable stream
 
 const converter = new SQLConverterStream({
-  numWorkers: argv['dir-output'] ? parseInt(argv.workers || 1, 10) : 1,
-  outputDir: argv['dir-output'],
-  schemaFormat: argv.schema,
+  outputDir,
+  numWorkers: outputDir ? parseInt(workers || 1, 10) : 1,
+  schemaFormat: schema,
 });
 
 
-process
-  .stdin
-  .pipe(converter)
-  .on('finish', () => {
-    process.exit();
-  });
+// Pipe file or stdin
+
+if (input) {
+  const filePath = path.resolve(__dirname, input);
+
+  fs.createReadStream(filePath)
+    .pipe(converter)
+    .on('finish', () => {
+      process.exit();
+    })
+    .on('error', (err) => {
+      throw err;
+    });
+} else {
+  process
+    .stdin
+    .pipe(converter)
+    .on('finish', () => {
+      process.exit();
+    });
+}
